ce89fc0cf11a9d8ec714ef1e44ded40aa4c11926
Arek <Arek@Arek-PC>
2021-04-13 08:08:06 +0200

    routing

--- app/controllers/CalcCtrl.class.php
+++ app/controllers/CalcCtrl.class.php
@@ -1,122 +1,137 @@
 <?php
-
 namespace app\controllers;
 
-use core\Messages;
+//zamieniamy zatem 'require' na 'use' wskazujÄ…c jedynie przestrzeĹ„ nazw, w ktĂłrej znajduje siÄ™ klasa
 use app\forms\CalcForm;
 use app\transfer\CalcResult;
 
 /** Kontroler kalkulatora
- * @author Przemysław Kudłacik
+ * @author PrzemysĹ‚aw KudĹ‚acik
  *
  */
 class CalcCtrl {
 
-	private $msgs;   //wiadomości dla widoku
-	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $form;   //dane formularza (do obliczeĹ„ i dla widoku)
 	private $result; //inne dane dla widoku
 
 	/** 
-	 * Konstruktor - inicjalizacja właściwości
+	 * Konstruktor - inicjalizacja wĹ‚aĹ›ciwoĹ›ci
 	 */
 	public function __construct(){
-		//stworzenie potrzebnych obiektów
-		$this->msgs = new Messages();
+		//stworzenie potrzebnych obiektĂłw
 		$this->form = new CalcForm();
 		$this->result = new CalcResult();
 	}
 	
 	/** 
-	 * Pobranie parametrów
+	 * Pobranie parametrĂłw
 	 */
 	public function getParams(){
-		$this->form->kwota = getFromRequest('kwota');
-		$this->form->lata = getFromRequest('lata');
-		$this->form->oproc = getFromRequest('oproc');
+		$this->form->x = getFromRequest('x');
+		$this->form->y = getFromRequest('y');
+		$this->form->op = getFromRequest('op');
 	}
 	
 	/** 
-	 * Walidacja parametrów
-	 * @return true jeśli brak błedów, false w przeciwnym wypadku 
+	 * Walidacja parametrĂłw
+	 * @return true jeĹ›li brak bĹ‚edĂłw, false w przeciwnym wypadku 
 	 */
 	public function validate() {
-		// sprawdzenie, czy parametry zostały przekazane
-		if (! (isset ( $this->form->kwota ) && isset ( $this->form->lata ) && isset ( $this->form->oproc ))) {
-			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+		// sprawdzenie, czy parametry zostaĹ‚y przekazane
+		if (! (isset ( $this->form->x ) && isset ( $this->form->y ) && isset ( $this->form->op ))) {
+			// sytuacja wystÄ…pi kiedy np. kontroler zostanie wywoĹ‚any bezpoĹ›rednio - nie z formularza
 			return false;
 		}
 		
-		// sprawdzenie, czy potrzebne wartości zostały przekazane
-		if ($this->form->kwota == "") {
-			getMessages()->addError('Nie podano kwoty');
+		// sprawdzenie, czy potrzebne wartoĹ›ci zostaĹ‚y przekazane
+		if ($this->form->x == "") {
+			getMessages()->addError('Nie podano liczby 1');
 		}
-		if ($this->form->lata == "") {
-			getMessages()->addError('Nie podano lat');
-		}
-                if ($this->form->oproc == "") {
-			getMessages()->addError('Nie podano oprocentowania');
+		if ($this->form->y == "") {
+			getMessages()->addError('Nie podano liczby 2');
 		}
 		
-		// nie ma sensu walidować dalej gdy brak parametrów
-		if (! $this->msgs->isError()) {
+		// nie ma sensu walidowaÄ‡ dalej gdy brak parametrĂłw
+		if (! getMessages()->isError()) {
 			
-			// sprawdzenie, czy $x i $y są liczbami całkowitymi
-			if (! is_numeric ( $this->form->kwota )) {
-                            getMessages()->addError('Kwota nie jest liczbą całkowitą');
+			// sprawdzenie, czy $x i $y sÄ… liczbami caĹ‚kowitymi
+			if (! is_numeric ( $this->form->x )) {
+				getMessages()->addError('Pierwsza wartoĹ›Ä‡ nie jest liczbÄ… caĹ‚kowitÄ…');
 			}
 			
-			if (! is_numeric ( $this->form->lata )) {
-				getMessages()->addError('Lata nie są liczbą całkowitą');
+			if (! is_numeric ( $this->form->y )) {
+				getMessages()->addError('Druga wartoĹ›Ä‡ nie jest liczbÄ… caĹ‚kowitÄ…');
 			}
-		}       if (! is_numeric ( $this->form->oproc )) {
-				getMessages()->addError('Oprocentowanie nie jest liczbą całkowitą');
-			}
+		}
 		
 		return ! getMessages()->isError();
 	}
 	
 	/** 
-	 * Pobranie wartości, walidacja, obliczenie i wyświetlenie
+	 * Pobranie wartoĹ›ci, walidacja, obliczenie i wyĹ›wietlenie
 	 */
-	public function process(){
-                global $role;
+	public function action_calcCompute(){
 
 		$this->getParams();
 		
 		if ($this->validate()) {
 				
-			//konwersja parametrów na int
-			$this->form->kwota = intval($this->form->kwota);
-			$this->form->lata = intval($this->form->lata);
-                        $this->form->oproc = intval($this->form->oproc);
-			$this->msgs->addInfo('Parametry poprawne.');
-                        
-                        $this->form->msc = $this->form->lata * 12;
-                        $this->form->opr = $this->form->oproc * 0.01;
+			//konwersja parametrĂłw na int
+			$this->form->x = intval($this->form->x);
+			$this->form->y = intval($this->form->y);
+			getMessages()->addInfo('Parametry poprawne.');
 				
 			//wykonanie operacji
-			 $this->result->result = ($this->form->kwota / $this->form->msc + ($this->form->kwota/$this->form->msc)*$this->form->oproc);
+			switch ($this->form->op) {
+				case 'minus' :
+					if (inRole('admin')) {
+						$this->result->result = $this->form->x - $this->form->y;
+						$this->result->op_name = '-';
+					} else {
+						getMessages()->addError('Tylko administrator moĹĽe wykonaÄ‡ tÄ™ operacjÄ™');
+					}
+					break;
+				case 'times' :
+					$this->result->result = $this->form->x * $this->form->y;
+					$this->result->op_name = '*';
+					break;
+				case 'div' :
+					if (inRole('admin')) {
+						$this->result->result = $this->form->x / $this->form->y;
+						$this->result->op_name = '/';
+					} else {
+						getMessages()->addError('Tylko administrator moĹĽe wykonaÄ‡ tÄ™ operacjÄ™');
+					}
+					break;
+				default :
+					$this->result->result = $this->form->x + $this->form->y;
+					$this->result->op_name = '+';
+					break;
+			}
 			
-			$this->msgs->addInfo('Wykonano obliczenia.');
+			getMessages()->addInfo('Wykonano obliczenia.');
 		}
 		
 		$this->generateView();
 	}
 	
+	public function action_calcShow(){
+		getMessages()->addInfo('Witaj w kalkulatorze');
+		$this->generateView();
+	}
 	
 	/**
 	 * Wygenerowanie widoku
 	 */
-	public function generateView(){;
-		
-		getSmarty()->assign('page_title','Kalkulator kredytowy');
-		getSmarty()->assign('page_description','Liczymy sobie');
-		getSmarty()->assign('page_header','Jest dobrze');
-					
-		getSmarty()->assign('msgs',$this->msgs);
+	public function generateView(){
+
+		getSmarty()->assign('user',unserialize($_SESSION['user']));
+				
+		getSmarty()->assign('page_title','Super kalkulator - role');
+
 		getSmarty()->assign('form',$this->form);
 		getSmarty()->assign('res',$this->result);
 		
-		getSmarty()->display('calc.tpl');
+		getSmarty()->display('CalcView.tpl');
 	}
 }
